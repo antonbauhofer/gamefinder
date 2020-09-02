92ff8cfbaf7c97339c6977588006b15d89e37172
Anton Bauhofer <48415604+antonbauhofer@users.noreply.github.com>
2020-09-02 22:34:53 +0200

    Update gamefinder.py
    Hide footer

--- gamefinder.py
+++ gamefinder.py
@@ -12,6 +12,7 @@ def main():
     hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
     """
     st.markdown(hide_menu_style, unsafe_allow_html=True)
